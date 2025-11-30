0c18c22c7863bb2cd6d86b5c98e47470a786ac3a
KonushkinMaksim <mkonushkinqa@gmail.com>
2025-11-30 21:26:27 +0400

    add getMessage() method

--- src/main/java/ru/Konushkin/Main.java
+++ src/main/java/ru/Konushkin/Main.java
@@ -6,8 +6,9 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome! qa.guru");
-
-
+        System.out.printf(getMessage());
+        }
+        private static String getMessage() {
+        return "Hello and welcome! qa.guru";
         }
     }
